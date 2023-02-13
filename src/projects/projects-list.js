9c0b37f2515a3cabed074ffab1404242038d49d2
Rabee Aljaramani <rabeejermani@gmail.com>
2023-02-13 18:33:05 +0400

    adding projects

--- src/projects/projects-list.js
+++ src/projects/projects-list.js
@@ -2,15 +2,35 @@ import it from '../assets/projects/it-img.jpg'
 import rabee_2022 from '../assets/projects/rabee-2022.jpg'
 import teacher_img from '../assets/projects/teacher-img.jpg'
 import mazyad_img from '../assets/projects/mazyad-img.jpg'
+import amazona_img from '../assets/projects/amazona-img.jpg'
+import roseshop_img from '../assets/projects/roseshop-img.jpg'
+import travel_img from '../assets/projects/travel-img.jpg'
+import resturant_img from '../assets/projects/resturant-img.jpg'
+import ag_img from '../assets/projects/ag-img.jpg'
+import agcts_img from '../assets/projects/agcts-img.jpg'
 
 
 const project_list = [
     {
-        proj_id: '1',
+        proj_id: 0,
+        proj_img: ag_img,
+        proj_name: 'Apparel Group',
+        proj_tech: 'WordPress',
+        proj_link: 'https://apparelgroupglobal.com/en/'
+    },
+    {
+        proj_id: 1,
         proj_img: rabee_2022,
         proj_name: 'My Portfolio 22',
         proj_tech: 'React-js',
         proj_link: 'https://rabe.netlify.app/'
+    },
+    {
+        proj_id: 11,
+        proj_img: agcts_img,
+        proj_name: 'AGCTS.NET',
+        proj_tech: 'WordPress',
+        proj_link: 'https://agcts.net/'
     },
     {
         proj_id: 2,
@@ -33,6 +53,33 @@ const project_list = [
         proj_tech: 'React-js',
         proj_link: 'https://mazyad-aljaramani.netlify.app/'
     },
-
+    {
+        proj_id: 5,
+        proj_img: amazona_img,
+        proj_name: 'E-commerce',
+        proj_tech: 'Next-js, React-js',
+        proj_link: 'https://rab.vercel.app/'
+    },
+    {
+        proj_id: 6,
+        proj_img: roseshop_img,
+        proj_name: 'E-Rose Shop',
+        proj_tech: 'React-js',
+        proj_link: 'https://rose-shop.netlify.app/'
+    },
+    {
+        proj_id: 7,
+        proj_img: travel_img,
+        proj_name: 'Travel Agency',
+        proj_tech: 'React-js',
+        proj_link: 'https://fastech-travelagency.netlify.app/'
+    },
+    {
+        proj_id: 8,
+        proj_img: resturant_img,
+        proj_name: 'Resturant',
+        proj_tech: 'React-js',
+        proj_link: 'https://beirut-lounge.netlify.app/'
+    },
 ]
 export default project_list;
