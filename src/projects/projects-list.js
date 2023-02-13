9f238aaf25df03730c9baf1e88ce594239ba9ea7
Rabee Aljaramani <rabeejermani@gmail.com>
2023-02-13 16:01:10 +0400

    add

--- src/projects/projects-list.js
+++ src/projects/projects-list.js
@@ -1,5 +1,7 @@
-import it from '../assets/projects/it-image.jpg'
+import it from '../assets/projects/it-img.jpg'
 import rabee_2022 from '../assets/projects/rabee-2022.jpg'
+import teacher_img from '../assets/projects/teacher-img.jpg'
+import mazyad_img from '../assets/projects/mazyad-img.jpg'
 
 
 const project_list = [
@@ -8,14 +10,28 @@ const project_list = [
         proj_img: rabee_2022,
         proj_name: 'My Portfolio 22',
         proj_tech: 'React-js',
-        proj_link: 'www.google.com'
+        proj_link: 'https://rabe.netlify.app/'
     },
     {
         proj_id: 2,
         proj_img: it,
         proj_name: 'IT Portfolio',
         proj_tech: 'React-js',
-        proj_link: 'www.google.com'
+        proj_link: 'https://jad-amer.netlify.app/'
+    },
+    {
+        proj_id: 3,
+        proj_img: teacher_img,
+        proj_name: 'Teacher Portfolio',
+        proj_tech: 'React-js',
+        proj_link: 'https://chri.netlify.app/'
+    },
+    {
+        proj_id: 4,
+        proj_img: mazyad_img,
+        proj_name: 'Photographer Portfolio',
+        proj_tech: 'React-js',
+        proj_link: 'https://mazyad-aljaramani.netlify.app/'
     },
 
 ]
